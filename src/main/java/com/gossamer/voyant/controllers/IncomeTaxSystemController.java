4af9ebf64c48640156e52bd81d92b26922cf2501
Nemo Robles <nemesisrobles@hotmail.com>
2025-08-25 14:12:12 -0500

    mvn clean package

--- src/main/java/com/gossamer/voyant/controllers/IncomeTaxSystemController.java
+++ src/main/java/com/gossamer/voyant/controllers/IncomeTaxSystemController.java
@@ -1,7 +1,6 @@
 package com.gossamer.voyant.controllers;
 
 import com.gossamer.voyant.entities.IncomeTaxBrackets;
-import com.gossamer.voyant.services.IncomeTaxSystemService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,41 +10,41 @@ import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
-@RequestMapping("incomeTax")
+@RequestMapping("connect")
 public class IncomeTaxSystemController {
-
-    private final IncomeTaxSystemService incomeTaxSystemService;
-
-    public IncomeTaxSystemController(IncomeTaxSystemService incomeTaxSystemService) {
-        this.incomeTaxSystemService = incomeTaxSystemService;
-    }
-
-    @GetMapping("/getAllIncomeTaxesBrackets")
-    List<IncomeTaxBrackets> getAllIncomeTaxBrackets() {
-        return incomeTaxSystemService.getAllTaxBrackets() ;
-    }
-
-    @GetMapping("/getIncomeTaxBracketsForIncome")
-    List<IncomeTaxBrackets> getIncomeTaxBracketsForIncome(@RequestParam BigDecimal income) {
-        return incomeTaxSystemService.getIncomeBracketsForIncomeLowToHigh(income) ;
-    }
-
-    @GetMapping("/calculateIncomeTax")
-    BigDecimal calculateIncomeTax(@RequestParam BigDecimal income) {
-
-        return incomeTaxSystemService.calculateIncomeTax(income) ;
-    }
-
-    @GetMapping("/determineMarginalTaxRate")
-    BigDecimal determineMarginalTaxRate(@RequestParam BigDecimal income) {
-
-        return incomeTaxSystemService.determineMarginalTaxRate(income) ;
-    }
-
-    @GetMapping("/determineEffectiveTaxRate")
-    BigDecimal determineEffectiveTaxRate(@RequestParam BigDecimal income) {
-
-        return incomeTaxSystemService.determineEffectiveTaxRate(income) ;
-    }
+//
+//    private final IncomeTaxSystemService incomeTaxSystemService;
+//
+//    public IncomeTaxSystemController(IncomeTaxSystemService incomeTaxSystemService) {
+//        this.incomeTaxSystemService = incomeTaxSystemService;
+//    }
+//
+//    @GetMapping("/getAllIncomeTaxesBrackets")
+//    List<IncomeTaxBrackets> getAllIncomeTaxBrackets() {
+//        return incomeTaxSystemService.getAllTaxBrackets() ;
+//    }
+//
+//    @GetMapping("/getIncomeTaxBracketsForIncome")
+//    List<IncomeTaxBrackets> getIncomeTaxBracketsForIncome(@RequestParam BigDecimal income) {
+//        return incomeTaxSystemService.getIncomeBracketsForIncomeLowToHigh(income) ;
+//    }
+//
+//    @GetMapping("/calculateIncomeTax")
+//    BigDecimal calculateIncomeTax(@RequestParam BigDecimal income) {
+//
+//        return incomeTaxSystemService.calculateIncomeTax(income) ;
+//    }
+//
+//    @GetMapping("/determineMarginalTaxRate")
+//    BigDecimal determineMarginalTaxRate(@RequestParam BigDecimal income) {
+//
+//        return incomeTaxSystemService.determineMarginalTaxRate(income) ;
+//    }
+//
+//    @GetMapping("/determineEffectiveTaxRate")
+//    BigDecimal determineEffectiveTaxRate(@RequestParam BigDecimal income) {
+//
+//        return incomeTaxSystemService.determineEffectiveTaxRate(income) ;
+//    }
 
 }
