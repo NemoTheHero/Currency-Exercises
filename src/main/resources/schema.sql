9da6b05879114bd9f91183af6519e362a5e613e9
Nemo <nemesisrobles@hotmail.com>
2025-08-25 14:51:41 -0500

    correcting sql ;

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -47,10 +47,10 @@ create table if not exists user_user_score
     PRIMARY KEY (ID),
     FOREIGN KEY (user1Id) references users (ID),
     FOREIGN KEY (user2Id) references users (ID)
-)
+);
 
-insert into users (ID, username) values (1, "Nemo")
-insert into users (ID, username) values (2, "Bailey")
-insert into users (ID, username) values (3, "Michael")
-insert into users (ID, username) values (4, "Karim")
-insert into users (ID, username) values (5, "Sam")
+insert into users (ID, username) values (1, "Nemo");
+insert into users (ID, username) values (2, "Bailey");
+insert into users (ID, username) values (3, "Michael");
+insert into users (ID, username) values (4, "Karim");
+insert into users (ID, username) values (5, "Sam");
