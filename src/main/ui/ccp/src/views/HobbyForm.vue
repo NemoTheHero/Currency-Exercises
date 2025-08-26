d6d4528b94c47857bb02aba5f4e44edee9867419
bsnyder <bsnyder@redoakcompliance.com>
2025-08-26 11:37:51 -0500

    HobbyForm updates

--- src/main/ui/ccp/src/views/HobbyForm.vue
+++ src/main/ui/ccp/src/views/HobbyForm.vue
@@ -2,58 +2,74 @@
 import {ref} from "vue";
 
 const hobbyInput = ref("");
-const suggestions = ref([
-  "Reading",
-  "Traveling",
-  "Cooking",
-  "Gardening",
-  "Photography",
-  "Painting",
-  "Hiking",
-  "Cycling",
-  "Fishing",
-  "Knitting",
-  "Dancing",
-  "Writing",
-  "Yoga",
-  "Bird Watching",
-  "Collecting (stamps, coins, etc.)"
-]);
+const correlatedHobbies = ref([]);
 
 const selections = ref<string[]>([]);
+
+const loadingSuggestions = ref(false);
 
 function clickAdd() {
   if (!hobbyInput.value || hobbyInput.value.trim() === "") {
     return;
   }
-  const newHobby = hobbyInput.value.trim();
-  selections.value.push(newHobby);
-  hobbyInput.value = "";
+  loadingSuggestions.value = true
 
-  fetch(`http://localhost:8080/recommendations/hobby?${new URLSearchParams({hobby: newHobby}).toString()}`).then(response => {
-    console.log("Response received:", response);
-  }).catch(error => {
-    console.error("Error adding hobby:", error);
-  });
+  const newHobby = hobbyInput.value.trim();
+  addHobby(newHobby)
+
+  fetch(`http://localhost:8080/recommendations/hobby?${new URLSearchParams({hobby: newHobby}).toString()}`)
+      .then(response =>
+          response.json())
+      .then(data => {
+        correlatedHobbies.value = data[newHobby.replaceAll(" ", "_")].correlated_hobbies;
+        console.log(data)
+      })
+      .catch(error => {
+        console.error("Error adding hobby:", error);
+      })
+      .finally(() => {
+        loadingSuggestions.value = false;
+      });
+
+  hobbyInput.value = "";
 }
 
-// function addHobby(value, weight) {
-//
-// }
+function addHobby(hobby) {
+  selections.value.push(hobby);
+  // update values in db
+}
+
+function clickChild(e, hobby) {
+  e.target.classList.add("selected");
+  e.target.style.pointerEvents = "none";
+  addHobby(hobby);
+}
+
+function clickParent(e, hobby) {
+  e.target.classList.add("selected");
+  e.target.style.pointerEvents = "none";
+  addHobby(hobby);
+}
 </script>
 
 <template>
   <div id="selections-box">
-    <span v-for="selection in selections">{{selection}}</span>
+    <span class="selection" v-for="selection in selections">{{selection}}</span>
   </div>
 <div id="hobby-input-wrapper" class="center-screen">
   <input v-model="hobbyInput" id="hobby-input" name="hobby" type="text" />
   <button @click="clickAdd">Add</button>
 </div>
-  <div id="suggestion-box">
-  <span v-for="suggestion in suggestions" :key="suggestion" class="suggestion">{{ suggestion }}
-
-  </span>
+  <div v-if="!loadingSuggestions" id="suggestion-box">
+  <div class="parent-suggestion-box" v-for="hobbyParent in correlatedHobbies" :key="hobbyParent.name">
+    <div class="parent-suggestion" @click="(e) => clickParent(e, hobbyParent.name)">{{ hobbyParent.name }}</div>
+    <div class="child-suggestion-box">
+    <div class="child-suggestion" v-for="hobbyChild in hobbyParent.correlated_hobbies" @click="(e) => clickChild(e, hobbyChild.name)"> {{ hobbyChild.name }}</div>
+      </div>
+  </div>
+  </div>
+  <div v-else>
+    Loading suggestions...
   </div>
   <div class="center-screen">
     <button>Done</button>
@@ -63,7 +79,7 @@ function clickAdd() {
 <style scoped>
 #suggestion-box {
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
   gap: 10px;
   margin-top: 20px;
   margin-bottom: 20px;
@@ -74,10 +90,12 @@ function clickAdd() {
   gap: 10px;
   margin-top: 20px;
   margin-bottom: 20px;
+  min-height: 200px;
 }
 
 #hobby-input {
   width: 300px;
+  margin-right: 6px;
 }
 
 .center-screen {
@@ -86,11 +104,35 @@ function clickAdd() {
   align-items: center;
 }
 
-.suggestion {
+.parent-suggestion-box {
+  text-align: center;
+}
+
+.parent-suggestion {
   border-radius: 6px;
   background-color: #313131;
   color: white;
+  padding: 4px;
+  text-align: center;
+  cursor: pointer;
+}
+
+.child-suggestion {
+  border-radius: 6px;
+  background-color: #656565;
+  color: white;
+  padding: 4px;
+  text-align: center;
+  margin: 5px;
+  cursor: pointer;
+}
+
+.selection {
+  border-radius: 20px;
+  background-color: #313131;
+  color: white;
   padding: 2px;
+  text-align: center;
 }
 
 input {
@@ -105,5 +147,10 @@ button {
   border-radius: 20px;
   background: #3232b8;
   color: white;
+  cursor: pointer;
+}
+
+.selected {
+  background-color: lightgray;
 }
 </style>
